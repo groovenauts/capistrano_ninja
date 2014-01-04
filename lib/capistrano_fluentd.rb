2ee6f5a0db562c01148163a757925ba6888ff88b
akima <akm2000@gmail.com>
2014-01-04 23:33:20 +0900

    add logging cap command arguments when cap command

--- lib/capistrano_fluentd.rb
+++ lib/capistrano_fluentd.rb
@@ -1,15 +1,20 @@
 require "capistrano_fluentd/version"
 
+require 'securerandom'
+
 module CapistranoFluentd
   autoload :Ext            , "capistrano_fluentd/ext"
   autoload :StaticTagLogger, "capistrano_fluentd/static_tag_logger"
 
   class << self
+    attr_accessor :tag, :command_id
+
     def config
       @config ||= {
         :tag  => "capistrano",
         :host => "localhost",
         :port => 24224,
+        :uuid => SecureRandom.uuid,
       }
     end
 
@@ -17,9 +22,19 @@ module CapistranoFluentd
       unless @logger
         c = config.dup
         tag = c.delete(:tag)
+        uuid = c.delete(:uuid)
         @logger = StaticTagLogger.new(tag, c)
+        @logger.extra.update(:uuid => uuid)
       end
       @logger
+    end
+
+    def configure
+      yield(self) if block_given?
+      Capistrano::Logger.module_eval do
+        include CapistranoFluentd::Ext::Logger
+      end
+      logger.post_without_tag({program: "capistrano", command: "#{$PROGRAM_NAME} #{ARGV.join(' ')}"})
     end
 
   end
--- lib/capistrano_fluentd/static_tag_logger.rb
+++ lib/capistrano_fluentd/static_tag_logger.rb
@@ -4,13 +4,17 @@ require 'fluent/logger'
 
 module CapistranoFluentd
   class StaticTagLogger < Fluent::Logger::FluentLogger
+    attr_reader :extra
+
     def initialize(tag, options = {})
       @tag = tag
+      @extra = {}
       super(nil, options)
     end
 
     def post_without_tag(map)
-      post(@tag, map)
+      m = extra.dup.update(map)
+      post(@tag, m)
     end
   end
 
